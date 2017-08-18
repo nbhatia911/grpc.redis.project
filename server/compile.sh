a7cd1f56630c32915e0b6974c5cb606042a6f3c0
Nikhil Bhatia <31141096+nbhatia911@users.noreply.github.com>
2017-08-19 01:20:55 +0530

    Update compile.sh

--- server/compile.sh
+++ server/compile.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+# use protoc compiler to generate pb.go file with grpc
 protoc -I ../proto/ ../proto/payment.proto --go_out=plugins=grpc:proto
+
+# compiles binary without docker
 go build -o server1 -v main.go
+
+# compiles binary with docker
 CGO_ENABLED=0 GOOS=linux go build -o server1_docker  -a -installsuffix cgo -v main.go 
+
+# creates docker image
 docker build -t my_grpc_server -f DockerfileServer .
