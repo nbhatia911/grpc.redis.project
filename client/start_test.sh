46a227b540b1e6fc25955aae136afd2a3c0f8121
Nikhil Bhatia <31141096+nbhatia911@users.noreply.github.com>
2017-08-19 01:31:41 +0530

    Update start_test.sh

--- client/start_test.sh
+++ client/start_test.sh
@@ -5,17 +5,17 @@ transaction_id=6217363
 
 while [ $i -lt 100 ]
 do  
-./client1 SET Nikhil${i} ${transaction_id}
-i=`expr $i + 1`
-transaction_id=`expr ${transaction_id} + 1`
+    ./client1 SET Nikhil${i} ${transaction_id}
+    i=`expr $i + 1`
+    transaction_id=`expr ${transaction_id} + 1`
 done
 
 i=0
 
 while [ $i -lt 100 ]
 do  
-./client1 GET Nikhil${i}
-i=`expr $i + 1`
+    ./client1 GET Nikhil${i}
+    i=`expr $i + 1`
 done
 
 
